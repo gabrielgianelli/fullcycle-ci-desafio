68a734b1550f140e9454afba004f0af465239f21
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-01-05 20:10:52 -0300

    feat: :sparkles: initialize project

---
+++ index.js
@@ -0,0 +1,6 @@
+const { add, subtract, multiply, divide } = require('./calculator.js');
+
+console.log(add(4,2));
+console.log(subtract(4,2));
+console.log(multiply(4,2));
+console.log(divide(4,2));
