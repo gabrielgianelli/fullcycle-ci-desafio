c353c57f8fd9350907c8cddc7b97a3becf39e49c
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-01-06 23:23:14 -0300

    test: :white_check_mark: create tests to subtract, multiply and divide

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,14 +4,14 @@ test('add', () => {
     expect(add(4, 2)).toBe(6);
 });
 
-// test('subtract', () => {
-//     expect(subtract(4, 2)).toBe(2);
-// });
+test('subtract', () => {
+    expect(subtract(4, 2)).toBe(2);
+});
 
-// test('multiply', () => {
-//     expect(multiply(4, 2)).toBe(8);
-// });
+test('multiply', () => {
+    expect(multiply(4, 2)).toBe(8);
+});
 
-// test('divide', () => {
-//     expect(divide(4, 2)).toBe(2);
-// });
+test('divide', () => {
+    expect(divide(4, 2)).toBe(2);
+});
